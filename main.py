41f39f251a24de97496bed30aa8b92cd99c07617
theoserrano <theorbserrano@gmail.com>
2025-06-03 20:07:05 -0300

    Função permutação

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-#teste para vocês verem
-print("THEO")
-print("ola")
+def permutacoes_n_digitos(n):
+    digitos = [str(i) for i in range(n)]
+    pilha = [("", digitos)]
+    resultado = []
+
+    while pilha:
+        prefixo, restantes = pilha.pop()
+
+        if len(prefixo) == n:
+            resultado.append(prefixo)
+        else:
+            for i in range(len(restantes)):
+                novo_prefixo = prefixo + restantes[i]
+                novo_restante = restantes[:i] + restantes[i+1:]
+                pilha.append((novo_prefixo, novo_restante))
+
+    return resultado
+
+# Exemplo:
+res = permutacoes_n_digitos(4)
+print(res)
+print("Total:", len(res))  # Deve imprimir 24 (4!)
+
